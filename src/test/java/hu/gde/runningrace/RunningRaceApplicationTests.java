9831b751a547d268002978f4c4ddba49ccfcc193
Varga Gábor <ARGVKW@neptun.gde.hu>
2024-12-15 02:47:52 +0100

    Create project

---
+++ src/test/java/hu/gde/runningrace/RunningRaceApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.gde.runningrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningRaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

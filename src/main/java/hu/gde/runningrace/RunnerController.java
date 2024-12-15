82a70ba79707af45c1dd662aaffe550ba83b64cd
Varga Gábor <ARGVKW@neptun.gde.hu>
2024-12-15 18:20:41 +0100

    Listing runners (route + template)

---
+++ src/main/java/hu/gde/runningrace/RunnerController.java
@@ -0,0 +1,24 @@
+package hu.gde.runningrace;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class RunnerController {
+
+    @Autowired
+    private RunnerRepository runnerRepository;
+    @Autowired
+    private ScoreRepository scoreRepository;
+
+    @GetMapping("/runners")
+    public String getAllRunners(Model model) {
+        List<RunnerEntity> runners = runnerRepository.findAll();
+        model.addAttribute("runners", runners);
+        return "runners";
+    }
+}
